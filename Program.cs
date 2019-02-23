a6e6b9a788e408c3995c372b7bcceab2b780d324
Vitaliy <vetal.t28@gmail.com>
2019-02-23 19:58:47 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class HomeWork2
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("\nEnter an integer vaule from 0 to 100 inclusive");
+            int[] arr = new int[100];
+            for (int i = 0; i < arr.Length - 1; i++) ;
+            Console.ReadLine();
+
+        }
+    }
+}
